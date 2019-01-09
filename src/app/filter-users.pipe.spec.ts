2a9dd31550b484f2c5fb2f0aead3d21b4bfc40f9
Morten <email>
2019-01-09 21:55:37 +0100

    Added more expects to custom pipe test

--- src/app/filter-users.pipe.spec.ts
+++ src/app/filter-users.pipe.spec.ts
@@ -8,19 +8,27 @@ fdescribe('FilterUsersPipe', () => {
 
 
     const users: User[] = [
-      {email: 'test@Te.dk', firstName: 'bar', lastName: 'testersen', password: 'tihi', originalEmail: 'test@Te.dk', isActive: true},
-      {email: 'test@te1.dk', firstName: 'foo', lastName: 'tustersen', password: 'tuhu', originalEmail: 'test@Te1.dk', isActive: true},
-      {email: 'test@Te2.dk', firstName: 'blob', lastName: 'blar', password: 'tihi', originalEmail: 'test@Te2.dk', isActive: true}
+      {email: 'morteno@mail.dk', firstName: 'Morten', lastName: 'Olsen', password: '123', originalEmail: 'morteno@mail.dk', isActive: true},
+      {email: 'dennisp@email.dk', firstName: 'Dennis', lastName: 'Petersen', password: 'letmein', originalEmail: 'dennisp@email.dk', isActive: true},
+      {email: 'christiank@mail.com', firstName: 'Christian', lastName: 'Kirschberg', password: 'hemmeligkode', originalEmail: 'christiank@mail.com', isActive: true}
     ];
 
     const userList = new MatTableDataSource(users);
 
     const filter: FilterUsersPipe = new FilterUsersPipe();
 
-    const result = filter.transform(userList, 'test@Te2.dk');
+    let result = filter.transform(userList, 'morteno@');
     console.log(result);
 
     expect(result.length).toBe(1);
-    expect(result[0].password).toBe('tihi');
+    expect(result[0].lastName).toBe('Olsen');
+
+    result = filter.transform(userList, '@mail');
+    expect(result.length).toBe(2);
+
+    result = filter.transform(userList, 'christian');
+    expect(result.length).toBe(1);
+    expect(result[0].lastName).toBe('Kirschberg');
+
   });
 });
